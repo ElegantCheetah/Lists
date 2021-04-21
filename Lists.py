522c2e5574f5b636ef5d16de9b3dd9a96d4270d2
ElegantCheetah <Zain4utah@gmail.com>
2021-04-21 13:32:36 -0600

    Official version 1.0.0 of Lists

--- Lists.py
+++ Lists.py
@@ -1,8 +1,14 @@
-#Zain Kergaye
-#Mr. Webster
-#Programming 2
-#4/21/21
+# Zain Kergaye
+# Mr. Webster
+# Programming 2
+# 4/21/21
 
-lists = [1,'Yes', 'true', '\n', 1.2585]
+lists = ['Burgers', 'Pizza', 'Shakes', 'Candy', 'Fries', 'Sandwhich', 'Salad', 'Fruit', 'Veggies', 'Water']
+
+list2 = ['Dr. Pepper', 'Sprite', 'Coke', 'Lemonade', 'Pepsi']
+
+lists.append(list2)
 
 print(lists)
+print(len(lists))
+print(type(lists))

6cf0b94729173a95d068db2ae196c9410debe14a
ElegantCheetah <Zain4utah@gmail.com>
2021-04-24 19:05:44 -0600

    Version 1.0.3 of lists

--- Lists.py
+++ Lists.py
@@ -3,6 +3,9 @@
 # Programming 2
 # 4/21/21
 
+import os
+os.system("pause")
+
 lists = ['Burgers', 'Pizza', 'Shakes', 'Candy', 'Fries', 'Sandwhich', 'Salad', 'Fruit', 'Veggies', 'Water']
 
 list2 = ['Dr. Pepper', 'Sprite', 'Coke', 'Lemonade', 'Pepsi']
@@ -11,8 +14,14 @@ for x in list2:
     lists.append(x)
 
 print(lists)
+os.system("pause")
 print(len(lists))
+os.system("pause")
 print(type(lists))
-
+os.system("pause")
 for lists in lists:
     print(lists)
+os.system("pause")
+print(lists[2:5])
+#os.system("pause")
+

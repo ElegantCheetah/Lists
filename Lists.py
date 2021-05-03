79a237bde1ffb56af479dd58c561153b6400471c
ElegantCheetah <Zain4utah@gmail.com>
2021-05-03 14:08:05 -0600

    Added spaces

--- Lists.py
+++ Lists.py
@@ -5,22 +5,38 @@
 
 import os
 
-lists = ['Burgers', 'Pizza', 'Shakes', 'Candy', 'Fries', 'Sandwhich', 'Salad', 'Fruit', 'Veggies', 'Water']
+lists = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-list2 = ['Dr. Pepper', 'Sprite', 'Coke', 'Lemonade', 'Pepsi']
+list2 = [11, 12, 13, 14, 15]
 
 for x in list2:
     lists.append(x)
 
 print(lists)
-os.system("pause")
-print(len(lists))
-os.system("pause")
-print(type(lists))
-os.system("pause")
-for lists in lists:
-    print(lists)
-os.system("pause")
-print(lists[2:5])
-#os.system("pause")
 
+os.system("pause")
+
+print(len(lists))
+
+os.system("pause")
+
+print(type(lists))
+
+os.system("pause")
+
+for lisxts in lists:
+    print(lisxts)
+
+os.system("pause")
+
+print(lists[3])
+
+os.system("pause")
+
+y = 0
+for x in lists:
+    y+=1
+    print(str(y) + '. ' + x)
+
+for list2, x in enumerate(lists):
+    print (lists, x)
